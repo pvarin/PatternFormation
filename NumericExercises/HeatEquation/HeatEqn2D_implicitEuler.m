cca87a68091be4e62ea1cdfdca0cc7a96a0c81e9
unknown <pvarin@JQJRTM1.olin.edu>
2012-05-23 23:57:57 -0500

    mostly working implicit Euler for the 2D heat equation, read the FIXME on line 19 for bug report

--- NumericExercises/HeatEquation/HeatEqn2D_implicitEuler.m
+++ NumericExercises/HeatEquation/HeatEqn2D_implicitEuler.m
@@ -7,24 +7,46 @@ k=25;
 
 % aribitrary initial condition
 [X,Y] = meshgrid(linspace(0,dx*N,N), linspace(0,dy*M,M));
-U0 = X.*Y.*sin(X/10).*sin(Y/10)/10000;
+V0 = X.*Y.*sin(X/10).*sin(Y/10)/10000;
 
 % initialize the matrix
-U = Matrix2Vector(U0);
+V = Matrix2Vector(V0);
 
-% construct the 2D Laplacian matrix
+% construct the 2D Laplacian matrix for Neumann boundary
+diag = zeros(N*M,1);
+diag(1:N) = [-2; -3*ones(N-2,1); -2];
+diag(end-N+1:end) = [-2; -3*ones(N-2,1); -2];
+temp = [-4; -4*ones(N-2,1); -4];%FIXME: I think this should  be
+                                %[-3; -4*ones(N-2,1); -3] but this gives
+                                %the wrong result, reanalyze the laplacian
+                                %matrix
+for i=2:M-1
+    diag(((i-1)*N+1):i*N) = temp;
+end
+B = [ones(N*M,1) ones(N*M,1) diag ones(N*M,1) ones(N*M,1)];
+d = [-N -1 0 1 N];
+L=spdiags(B,d,N*M,N*M);
+[V_L, V_U, ~] = lu(speye(N*M) - k*dt/dx^2*L);
 
 figure(1)
+
+surf(linspace(0,dx*N,N),linspace(0,dx*M,M),Vector2Matrix(V,N,M))
+xlabel('x')
+ylabel('y')
+shading interp
+axis([0 N*dx 0 M*dy -.2 .8 -.2 .8])
+drawnow
 
 %Implicit Euler Method
 for i=2:numsteps
     
+    V = V_U\(V_L\V);
     
-    
-    surf(linspace(0,dx*N,N),linspace(0,dx*M,M),Vector2Matrix(U,N,M))
+    surf(linspace(0,dx*N,N),linspace(0,dx*M,M),Vector2Matrix(V,N,M))
     xlabel('x')
     ylabel('y')
     shading interp
     axis([0 N*dx 0 M*dy -.2 .8 -.2 .8])
     drawnow
+%     return
 end
