3d8b5663c583230bcf956cd1cbe79734235ebad7
pvarin <patrick.varin@students.olin.edu>
2012-05-24 22:22:39 -0500

    saved high res data for a numerical solution to the Chemotaxis equations using a partial Crank-Nicolson method

---
+++ Chemotaxis/Chemotaxis1D_CrankNicolson.m
@@ -0,0 +1,55 @@
+% model parameters
+b = 1.01;
+kappa = 1;
+
+% simulation parameters
+dt = .0001; T = 80;
+dx = 1;
+numsteps = T/dt;
+N = 100;
+
+% initial conditions
+U0 = ones(N,1); U0(1) = U0(1) + .1
+V0 = ones(N,1);
+
+% initialize the matrices
+U = zeros(N,numsteps); V = zeros(N,numsteps);
+U(:,1) = U0; V(:,1) = V0;
+
+%construct derivative matrix
+B = [-ones(N,1) ones(N,1)];
+d = [-1; 1];
+D = spdiags(B, d, N, N);
+D(1,1) = -1;
+D(N,N) = 1;
+D = D/dx;
+
+% construct laplacian matrix
+uDiag = [0; ones(N-1,1)];
+cDiag = [-1;-2*ones(N-2,1);-1];
+lDiag = [ones(N-1,1); 0];
+B = [uDiag, cDiag, lDiag];
+d = [1; 0; -1];
+L = spdiags(B, d, N, N)/dx^2;
+
+[L_U, U_U, ~] = lu(speye(N)-dt/2*L);
+[L_V, U_V, ~] = lu(speye(N)-dt/2*(kappa*L-speye(N)));
+
+U_op = speye(N)+dt/2*L;
+V_op = speye(N)+dt/2*(kappa*L-speye(N));
+
+%Explicit Euler Method
+for i=2:numsteps
+    U_op*U(:,i-1);
+    U(:,i) = U_U\(L_U\(U_op*U(:,i-1)-dt*b*D*(D*V(:,i-1).*U(:,i-1))));
+    V(:,i) = U_V\(L_V\(V_op*V(:,i-1)+dt*U(:,i-1)));
+end
+
+figure
+pcolor(linspace(0,dx*N,N),linspace(0,dt*numsteps,100),U(:,1:floor(end/100):end)')
+shading flat
+colormap gray
+drawnow
+axis([0 dx*N 0 T 0 .1 0 1])
+    
+animate(U,V,dx)
