051aa23eacbcb92a89f2713bfa68dc2f0af57a9d
pvarin <patrick.varin@students.olin.edu>
2012-06-15 16:58:54 -0500

    committing changes for today, yet another unhelpful commit message

--- Chemotaxis/Chemotaxis2DPatterns/BVP_Solver/chemsolve.m
+++
@@ -1,8 +0,0 @@
-function vsol=chemsolve(initial,N,mu,L,kappa)
-% finds the solutions to the boundary value problem defined in chembvp using fsolve
-% given an initial guess initial, number of points N, parameter and length L
-% 
-
-options = optimset('TolX',1e-8);
-vsol=fsolve(@(v) chembvp(v,N,mu,L,kappa),initial,options);
-end
