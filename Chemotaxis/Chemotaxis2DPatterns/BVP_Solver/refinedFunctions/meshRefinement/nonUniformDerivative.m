e74fb81cf38402ea1d11c7855fd7e6dc456aea4b
pvarin <patrick.varin@students.olin.edu>
2012-06-20 18:05:48 -0500

    committing for the day, maybe a one sided (left) finite difference scheme is the best way to go

--- Chemotaxis/Chemotaxis2DPatterns/BVP_Solver/refinedFunctions/meshRefinement/nonUniformDerivative.m
+++ Chemotaxis/Chemotaxis2DPatterns/BVP_Solver/refinedFunctions/meshRefinement/nonUniformDerivative.m
@@ -9,7 +9,7 @@ function D = nonUniformDerivative(x,Li,Lf,bc)
 %     upperDiag = 1./[0; dx(1:end-1)];
 %     
     
-    
+    % assumes that the boundary is midway between two mesh points
     h = [2*(x(1)-Li); x(2:end)-x(1:end-1); 2*(Lf-x(end))];
     
     % first order terms
@@ -19,16 +19,16 @@ function D = nonUniformDerivative(x,Li,Lf,bc)
     
     % second order terms
     upperDiag = upperDiag + 2*(h(2:end)-h(1:end-1))./ ...
-                            (h(2:end)+h(1:end-1))./ ...
-                            h(2:end);
+                            (h(2:end)+h(1:end-1)).* ...
+                            (1./h(2:end));
                         
     centerDiag = centerDiag + -2*(h(2:end)-h(1:end-1))./ ...
                               (h(2:end)+h(1:end-1)).* ...
                               (1./h(2:end)+1./(h(1:end-1)));
     
     lowerDiag = lowerDiag + 2*(h(2:end)-h(1:end-1))./ ...
-                            (h(2:end)+h(1:end-1))./ ...
-                            h(1:end-1);
+                            (h(2:end)+h(1:end-1)).* ...
+                            (1./h(1:end-1));
     
     % Restructure Diagonals and Save Boundary Data
     initialBoundaryValue = lowerDiag(1);
@@ -47,8 +47,8 @@ function D = nonUniformDerivative(x,Li,Lf,bc)
             D(end,end) = D(end,end) + finalBoundaryValue;
         case 'DIRICHLET'
             % Homogeneous Dirichlet Boundary Conditions
-            D(1,1) = D(1,1) - initialBoundaryValue;
-            D(end,end) = D(end,end) - finalBoundaryValue;
+%             D(1,1) = D(1,1) - initialBoundaryValue;
+%             D(end,end) = D(end,end) - finalBoundaryValue;
         case 'PERIODIC'
             % Periodic Boundary Conditions
             D(1,end) = D(1,end) + initialBoundaryValue;
