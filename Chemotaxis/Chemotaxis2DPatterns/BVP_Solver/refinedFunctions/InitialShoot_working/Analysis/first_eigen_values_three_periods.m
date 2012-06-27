5939a2661d6e4da0def20cccca6a4e425b33dd63
pvarin <patrick.varin@students.olin.edu>
2012-06-27 17:24:38 -0500

    learning alot about the unstable eigenfunctions of the linearized chemotaxis equations

---
+++ Chemotaxis/Chemotaxis2DPatterns/BVP_Solver/refinedFunctions/InitialShoot_working/Analysis/first_eigen_values_three_periods.m
@@ -0,0 +1,35 @@
+load('../Data/equilibriumSolutions_kappa_10_mass_11_20.mat')
+
+%two periods
+evals = zeros(length(U(1,:)),4);
+
+for i = 1:length(U(1,:));
+    u2 = [U(:,i); U(end:-1:1,i); U(:,i)];
+    v2 = [V(:,i); V(end:-1:1,i); V(:,i)];
+    Lin = chemLinear(u2,v2,periods(i)*3,kappa);
+    [~,eVals] = eigs(Lin,10,3);
+    eVals = sort(diag(eVals),1,'descend');
+    evals(i,:) = eVals(1:4);
+    i
+end
+
+masses_ = masses;
+load('../Data/equilibriumSolutions_kappa_10_mass_20_50.mat')
+evals = [evals;zeros(length(U(1,:)),4)];
+
+for i = 1:length(U(1,:));
+    u2 = [U(:,i); U(end:-1:1,i); U(:,i)];
+    v2 = [V(:,i); V(end:-1:1,i); V(:,i)];
+    Lin = chemLinear(u2,v2,periods(i)*3,kappa);
+    [~,eVals] = eigs(Lin,10,3);
+    eVals = sort(diag(eVals),1,'descend');
+    evals(i+length(masses_),:) = eVals(1:4);
+    i+length(masses_)
+end
+
+masses = [masses_;masses];
+plot(masses,evals','.')
+xlabel('mass')
+ylabel('eigenvalues')
+
+save('Coarsening3Periods.mat','masses','evals')
